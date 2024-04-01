ff5df96c7d6a5228596d679be669fcda397c2979
orukahairuka <sakuraierika03301109@gmail.com>
2024-04-01 17:31:26 +0900

    Initial Commit

---
+++ okashi/okashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  okashiApp.swift
+//  okashi
+//
+//  Created by 櫻井絵理香 on 2024/04/01.
+//
+
+import SwiftUI
+
+@main
+struct okashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
