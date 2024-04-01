99462f458950b6da4bbf979ff3c74c3ae35c29e7
orukahairuka <sakuraierika03301109@gmail.com>
2024-04-01 20:45:42 +0900

    wip

--- okashi/OkashiData.swift
+++ okashi/OkashiData.swift
@@ -12,5 +12,16 @@ class OkashiData: ObservableObject {
     //WebAPI検索用メソッド 第一引数:keyword 検索したいワード
     func searchOkashi(keyword: String) {
         print("searchOkashiメソッドで受け取った値：\(keyword)")
+
+        //Taskは非同期処理を実行できる
+        Task {
+            await searchOkashi(keyword: keyword)
+        }
     }
+
+        //非同期処理でお菓子データを取得
+        private func search(keyword: String) async {
+            print("searchメソッドで受け取った値\(keyword)")
+        }
+
 }
