2f2c46abe46e2310fce9a523e72898dacd06233c
orukahairuka <sakuraierika03301109@gmail.com>
2024-04-03 20:51:11 +0900

    git no api

--- okashi/ContentView.swift
+++ okashi/ContentView.swift
@@ -1,4 +1,3 @@
-import SwiftUI
 
 
 import SwiftUI
@@ -6,22 +5,40 @@ import SwiftUI
 struct GithubUserView: View {
     private var viewModel = GithubUserViewModel()
     @State private var users: [GithubUserViewModel.User.Item] = []
+    @State private var inputText: String = "tarou"
 
     var body: some View {
-        NavigationView {
-            List(users, id: \.id) { user in
-                VStack(alignment: .leading) {
-                    Text(user.login)
-                        .font(.headline)
-                    Text(user.html_url.absoluteString)
-                        .font(.subheadline)
-                }
-            }
-            .navigationBarTitle("GitHub Users")
-            .onAppear {
-                viewModel.searchGithubUser(query: "tarou") { fetchedUsers in
+        VStack {
+            Spacer()
+            TextField("名前検索するよ", text: $inputText)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            Button {
+                viewModel.searchGithubUser(query: inputText) { fetchedUsers in
                     self.users = fetchedUsers
                 }
+            } label: {
+                Text("検索する")
+            }
+            .frame(width: 130,height:  90)
+            .background(.blue)
+            .foregroundColor(.white)
+            .cornerRadius(15)
+            NavigationView {
+                List(users, id: \.id) { user in
+                    VStack(alignment: .leading) {
+                        Text(user.login)
+                            .font(.headline)
+                        Text(user.html_url.absoluteString)
+                            .font(.subheadline)
+                    }
+                }
+                .navigationBarTitle("GitHub Users")
+//                .onAppear {
+//                    viewModel.searchGithubUser(query: inputText) { fetchedUsers in
+//                        self.users = fetchedUsers
+//                    }
+//                }
             }
         }
     }
