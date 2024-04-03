776590623a3ebd79afe00804208514883013c2ba
orukahairuka <sakuraierika03301109@gmail.com>
2024-04-03 12:02:00 +0900

    Qiitaのapiは撮って来れるらしい

--- okashi/okashiApp.swift
+++ okashi/okashiApp.swift
@@ -5,13 +5,3 @@
 //  Created by 櫻井絵理香 on 2024/04/01.
 //
 
-import SwiftUI
-
-@main
-struct okashiApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
