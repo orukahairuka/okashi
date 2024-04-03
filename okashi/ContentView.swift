8a344c5855df95bec2e4872f6ecb1e9b0f79ed74
orukahairuka <sakuraierika03301109@gmail.com>
2024-04-03 16:35:15 +0900

    wip

--- okashi/ContentView.swift
+++ okashi/ContentView.swift
@@ -1,77 +1,42 @@
 import SwiftUI
-import Combine
 
-// Make sure QiitaViewModel is an ObservableObject
-class QiitaViewModel: ObservableObject {
-    @Published var articles: [QiitaStruct] = []
 
-    init() {
-        fetchArticle()
-    }
+import SwiftUI
 
-    func fetchArticle() {
-        let url = "https://qiita.com/api/v2/items"
-
-        guard var urlComponents = URLComponents(string: url) else {
-            return
-        }
-
-        urlComponents.queryItems = [
-            URLQueryItem(name: "per_page", value: "50"),
-        ]
-
-        let task = URLSession.shared.dataTask(with: urlComponents.url!) { [weak self] data, response, error in
-            guard let jsonData = data else {
-                return
-            }
-
-            do {
-                let articles = try JSONDecoder().decode([QiitaStruct].self, from: jsonData)
-                DispatchQueue.main.async {
-                    self?.articles = articles
-                }
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
-        task.resume()
-    }
-}
-
-// SwiftUI ContentView
-struct ContentView: View {
-    @ObservedObject var viewModel = QiitaViewModel()
+struct GithubUserView: View {
+    private var viewModel = GithubUserViewModel()
+    @State private var users: [GithubUserViewModel.User.Item] = []
 
     var body: some View {
         NavigationView {
-            List(viewModel.articles, id: \.title) { article in
+            List(users, id: \.id) { user in
                 VStack(alignment: .leading) {
-                    Text(article.title)
+                    Text(user.login)
                         .font(.headline)
-                    Text(article.user.name)
+                    Text(user.html_url.absoluteString)
                         .font(.subheadline)
                 }
             }
-            .navigationBarTitle("QiitaAPI")
+            .navigationBarTitle("GitHub Users")
+            .onAppear {
+                viewModel.searchGithubUser(query: "tarou") { fetchedUsers in
+                    self.users = fetchedUsers
+                }
+            }
         }
     }
 }
 
-// Your QiitaStruct remains unchanged
-struct QiitaStruct: Codable {
-    var title: String
-    var user: User
-    struct User: Codable {
-        var name: String
-    }
-}
+
+
+
 
 // SwiftUI App Entry Point
 @main
 struct YourApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GithubUserView()
         }
     }
 }
